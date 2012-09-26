0391da1c79783c8f754ddea9b00239f965e7d061
marqh <markh@scitools.org.uk>
2012-09-26 12:18:13 +0000

    first iteration of two flag save cache process

---
+++ tdbSaveCache.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export metOcean="/net/home/h04/itmh/metarelate/metOcean-mapping"
+
+$JENAROOT/bin/tdbquery --loc=$metOcean/metocean_store --query=saveCacheQuery.qu > $metOcean/default/zzztmp.ttl
+
+mdOut=$(md5sum $metOcean/default/zzztmp.ttl) 
+
+md=$(echo $mdOut | cut -d' ' -f1)
+
+mv ${metOcean}/default/zzztmp.ttl ${metOcean}/default/${md}.ttl
+

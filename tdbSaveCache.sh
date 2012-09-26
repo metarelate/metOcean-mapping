844c307ac3ae1e9f7e696bfa317cf9b2975974e0
marqh <markh@scitools.org.uk>
2012-09-26 15:48:19 +0000

    one flag solution, using delete after conditional dump

--- tdbSaveCache.sh
+++ tdbSaveCache.sh
@@ -3,6 +3,7 @@
 export metOcean="."
 
 $JENAROOT/bin/tdbquery --loc=$metOcean/metocean_store --query=saveCacheQuery.qu > $metOcean/default/zzztmp.ttl
+$JENAROOT/bin/tdbupdate --loc=$metOcean/metocean_store --update=clearCacheQuery.qu
 
 mdOut=$(md5sum $metOcean/default/zzztmp.ttl) 
 
